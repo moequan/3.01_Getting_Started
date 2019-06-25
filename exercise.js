78465b959aa263076441f6a08cfac6e04c09ed7f
moequan <maurice.s93@web.de>
2019-06-25 10:19:20 +0200

    Solving Exercise_1

---
+++ exercise.js
@@ -0,0 +1,36 @@
+
+//1.Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let myName="Hallihallo";
+console.log (myName);
+
+//2.Access the 2nd and 6th elements of the string. Print the elements to the console. **Make sure you have selected the right index numbers**
+console.log (myName[1], myName[5]);
+
+//3. Declare a variable called name and assign a string as a value.
+let name="Maurice";
+
+//4. Declare a variable called age and assign a number as a value. 
+let age=25;
+
+//5. Declare a variable called year and assign a number as a value.
+let year=2019; 
+
+//6. With your declared variables, print the following: "/name/ is /age/ in /year/".
+console.log(name+ " is " + age+ " in "+year);
+
+//7. Declare a variable called isMarried and assign a boolean value.
+let isMarried=false;
+
+//8. With your declared variables, print the following "/name/ is married: true/false".
+console.log (isMarried);
+
+//9. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true.  Print "JavaScript is fun: true" to the console.
+let programmingLanguage="JavaScript";
+let isFun=true;
+console.log (programmingLanguage + " is fun :",[isFun]);
+
+//10. Print the J and S of JavaScript from the variable programmingLanguage to the console.
+console.log (programmingLanguage[0], programmingLanguage[4]);
+
+//11. Print the length of the string "JavaScript" to the console.
+console.log (programmingLanguage.length);
