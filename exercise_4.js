58ab8e7e6c796add5935ca4440258cc818fc58a6
moequan <maurice.s93@web.de>
2019-06-27 13:32:24 +0200

    Solving Exercise_4.js (Operators)

---
+++ exercise_4.js
@@ -0,0 +1,45 @@
+//**Operators Exercises**
+
+
+//These exercises are aimed at making you familiar with the aritmetic operators. Print all your results to the console.
+
+//Declare two variables "x" and "y" and assign number values.
+
+
+//1. Add x and y, and print the result to the console.
+
+var x = 10;
+var y = 15;
+console.log(x,y)
+
+//2. Subtract y from x, and print the result to the console. Then, subtract x from y and print the result to the console.
+
+console.log(x-y); //-5
+console.log(y-x) //5
+
+//3. Multiply x and y, and print the result to the console.
+
+console.log(x*y)    //150
+
+//4. Divide x and y, and print the result to the console. 
+
+console.log(15/10) //1.5
+
+//5. Declare another variable "z" with the value "10". Multiply x and y. Then, divide the result by z. Store the result in a new variable named "resultOne". Print "result" to the console.
+
+var z = 10;
+var resultOne = ((y*x)/10);
+console.log(resultOne);
+
+//5. Declare two variables "a" with the value of 15 and "b" with the value of 9. Print the remainder when a is divided by b. 
+
+var a = 15;
+var b = 9;
+console.log(15/9); //1.667 / 1/3
+
+//6. Declare another variable "c" with the value of 20. Add a and b, then multiply the result by c. Store the result in variable "resultTwo".  Print "resultTwo" to the console.
+
+var c = 20; 
+resultTwo = ((15+9)*20);
+console.log(resultTwo)
+
