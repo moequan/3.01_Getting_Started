7c7903b0837a70fdf5135082c61c28619812981f
moequan <maurice.s93@web.de>
2019-06-27 13:12:45 +0200

    AND/OR , Logical operators, Mathematical operators  THE TERNARY OPERATOR and Escaping spiecial characters.

--- operators.js
+++ operators.js
@@ -16,5 +16,37 @@ let ageVlad = 32;
 console.log (`Am I younger than Vlad" ${myAge < ageVlad}`)
 console.log (`Am I younger than Vlad" ${myAge <= ageVlad}`)
 console.log (`Am I younger than Vlad" ${myAge === ageVlad}`)
+console.log (`Am I younger than Vlad" ${myAge >= ageVlad}`)
+console.log (`Am I younger than Vlad" ${myAge !== ageVlad}`)
+
+//Logical operators (AND/OR)            AND=&& ; OR=||
+
+
+//AND
+
+let password=true; 
+let username=true; 
+
+let login = (password && username);
+console.log(`Can I log In? : ${login}`);
+
+let age = 25;
+let validId=true;
+
+
+let enterTheClub = (age >= 18) && (validId);
+console.log(`Can I enter the Club? : ${enterTheClub}`)
+
+//OR
+
+let wig=true; 
+let ID=true;
+let TheClub = wig || ID;
+console.log(TheClub);
+
+let newClub = ((age >= 18) && (ID)) || wig; 
+console.log(newClub)
+
+
 
 
