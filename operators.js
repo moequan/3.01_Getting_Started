11899670395d9411145046188e42f310a1f21410
moequan <maurice.s93@web.de>
2019-06-27 11:45:05 +0200

    Learning operators.

---
+++ operators.js
@@ -0,0 +1,20 @@
+//Escaping spiecial characters
+
+console.log ( 'Hi there my name is D\'Angelo') // "\" is solving the errors.
+
+// THE TERNARY OPERATOR
+
+let sunny = true;
+let happy =  sunny ? 35 : 12; 
+console.log (`Is it sunny?" : ${sunny}`)
+console.log (`Temperatur" : ${happy}`)
+
+//Mathematical operators 
+
+let myAge = 25;
+let ageVlad = 32;
+console.log (`Am I younger than Vlad" ${myAge < ageVlad}`)
+console.log (`Am I younger than Vlad" ${myAge <= ageVlad}`)
+console.log (`Am I younger than Vlad" ${myAge === ageVlad}`)
+
+
