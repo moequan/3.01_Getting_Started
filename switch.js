9be4f3c60c0e2b261f98ed12464885921f4eda7d
moequan <maurice.s93@web.de>
2019-07-03 11:18:43 +0200

    Learning switch.

---
+++ switch.js
@@ -0,0 +1,64 @@
+ let age = 11; 
+/* if(age < 10) {
+    console.log (`I am a kiddo`)
+}
+else if (age > 10 && age < 40){
+    console.log(`I am not a kiddo for sure!`)
+}
+else if (age > 50){
+    console.log(`I have life figured out!`)
+}
+else console.log(`You are something else!`)
+ */
+ 
+//Switch
+
+
+
+
+/* switch(true) {
+    case(age < 10):
+        console.log(`I am a kiddo`);
+        break;
+    case(age > 10 && age < 40):
+        console.log(`I am not a kiddo`);
+        break;
+        
+    case(age > 50):
+    console.log(`I have life figured out!`);
+    break;
+    default: console.log(`You are something else`);
+    
+} */
+
+/* switch(age){
+    case(10):
+        console.log(`You are 10 years old`);
+        break;
+    case(20):
+    console.log(`You are 20 years old`);
+        break;
+    case(50):
+        console.log(`You are 50 years old`);
+        break;
+    default: 
+    console.log(`you are ${age} years old`);
+} */
+
+//while
+
+ do {
+    if(age%5==0){
+        console.log(`Your age is ${age}.`)
+    }
+    age++
+} while(age < 100) 
+ 
+//Infinite Loop
+
+
+/* do {
+    age++;
+    console.log(`You are ${age} years old`)
+} while(age > 0)
+ */
