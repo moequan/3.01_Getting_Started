73991644bfb37835030a68b0441640b030252fb6
moequan <maurice.s93@web.de>
2019-07-08 11:08:54 +0200

    Changing issues.

--- exercise_11.js
+++ exercise_11.js
@@ -1,93 +1,151 @@
-//1 
-let num = 1
-console.log(12345678910)
-for (let i = 1; i <= 10; i++) {
-    console.log(i)
-}
-console.log("--------------------------------")
-//2 
-let string = "*";
-for (let i = 0; i < 5; i++) {
-    console.log(string)
-    string = string + "*"
-}
+// //1 
+// let num = 1
+// console.log(12345678910)
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+// console.log("--------------------------------")
+// //2 
+// let string = "*";
+// for (let i = 0; i < 5; i++) {
+//     console.log(string)
+//     string = string + "*"
+// }
 
-console.log("--------------------------------")
+// console.log("--------------------------------")
 
-for (let i = 1; i <= 20; i++) {
-    console.log(`This is iteration number ${i}`)
-}
+// for (let i = 1; i <= 20; i++) {
+//     console.log(`This is iteration number ${i}`)
+// }
 
-console.log("--------------------------------")
+// console.log("--------------------------------")
 
-for (let i = 1; i <= 20; i++) {
-    if (i % 2 === 0)
-        console.log(`EVEN WUUUP WUUUP ${i}`)
-    else {
-        console.log(`ODDDDDDD${i} `)
-    }
-}
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0)
+//         console.log(`EVEN WUUUP WUUUP ${i}`)
+//     else {
+//         console.log(`ODDDDDDD${i} `)
+//     }
+// }
 
-console.log("--------------------------------")
+// console.log("--------------------------------")
 
-for (let i = 0; i <= 100; i++) {
-    if (i % 6 == 0)
-        console.log(` UUUUH, I am perfect :): ${i}`)
-    else {
-        console.log(`${i} I am ugly :(`)
-    }
-}
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 6 === 0)
+//         console.log(` UUUUH, I am perfect :): ${i}`)
+//     else {
+//         console.log(`${i} I am ugly :(`)
+//     }
+// }
 
-console.log("--------------------------------")
+// console.log("--------------------------------")
 
-var babylon = "BA-" 
+// // //var babylon = "BA-" 
 
-for(let i = 1; i < 5; i += 1) {
-  babylon=babylon + "BA-"
-}
-console.log(babylon + "BY-LON");
+// // for(let i = 1; i < 5; i += 1) {
+// //   babylon=babylon + "BA-"
+// // }
+// // console.log(babylon + "BY-LON");
 
-console.log("--------------------------------")
+// // let babylon = "";
 
-for (let i = 0; i < 3; i++) {
-    console.log(` I am a normal for loop ${i}.`);
-   ;
-    for( let j = 0; j<=2; j++)
-    console.log ( `I am a nested a for loop ${j}`)
-}
+// // for (i = 0; i < 7; i++)
+// // {
+// //     if ( i !== 6) {
+// //         if ( i !==5) {
+// //             babylon += "BA-"
+// //         }
+// //         else {
+// //             babylon += "BY-"
+// //         }
+// //     } else {
+// //         babylon += "LON"
+// //     }
 
-console.log("--------------------------------")
+// // }console.log(babylon)
+// // BA-BA-BA-BA-BA-BY-LON (5 BAs)
+// var str = '';
+// for (var i = 1; i < 7; i++) {
+//    if (i < 6) {
+//    str += 'BA-';
+//    } else if (i == 6) {
+//        str += 'BY-';
+//    } else{
+//        str += 'LON';
+//        console.log(str);
+//    }
+// }
 
-let star ='';
 
-for (let i=2; i<=2; i++) {
-   star = star + '*';
-   console.log(star)
-   for (let j=0; j<=2; j++) {
-   star = star + '!'
-   console.log(star)
-}
-}
 
-console.log("--------------------------------")
+// console.log("--------------------------------")
 
-let  num1=2;  num2=900;
+// for (let i = 0; i <= 2; i++) {
+//     console.log(` I am a normal for loop ${i}.`);
+//    ;
+//     for( let j = 0; j<=2; j++)
+//     console.log ( `I am a nested  for loop ${j}`)
+// }
 
-for (let i = 2; i <= 4; i++) {
-    console.log(i);
-   ;
-    for( let j = 0; j <= 2; j++){
-        num2++
-        console.log (num2)
+// console.log("--------------------------------")
+
+// let star ='';
+
+// for (let i=0; i<=2; i++) {
+//    star = star + '*';
+//    console.log(star)
+//    for (let j=0; j<=2; j++) {
+//    star = star + '!'
+//    console.log(star)
+// }
+// }
+
+// // var str = '';
+// // for (var i = 1; i < 4; i++) {
+// //    str += '*';
+// //    console.log(str);
+// //    for (var j = 1; j < 4; j++) {
+// //        str += '!'
+// //        console.log(str);
+// //    }
+// // }
+
+// console.log("--------------------------------")
+
+// let  num1=1;  num2=900;
+
+// for (let i = 0; i <= 2; i++) {
+//     num1=num1+1; //num1++;
+//     console.log(num1);
+//     for( let j = 0; j <= 2; j++){
+//         num2++
+//         console.log (num2)
        
-    }
-    }
+//     }
+//     }
 
-    console.log("--------------------------------")
+//     console.log("--------------------------------")
 
-    let star2 = "*" ;
-    let star1 = "*****";
-for (let i = 5; i >= 0; i--) {
-    star1-star2
-    console.log(star1)}
+//      let star2 = "*" ;
+//    let star1 = "*****";
+//     let result = star1 - star2
+// for (let i = 5; i >= 0; i--) {
+//      star1-star2
+//    console.log(result)}
+
+
+let result_star=``;
+for( let i=5; i>=1; i--)
+{
+    
+    for(let j=1; j<=i; j++)
+    {
+        result_star= result_star + `*`;  
+    }
+    console.log(result_star);
+    result_star=``;
+}
+
+
+
   
